15fd607ba8825bf3ba4bc4cd9baf70b2f5d68f5f
Your Name <you@example.com>
2022-02-18 23:37:32 +0300

    responsive design updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import Content from "./Components/Content";
 import Header from "./Components/Header";
-import styled from 'styled-components'
-import { Provider } from 'react-redux'
+import styled from "styled-components";
+import { Provider } from "react-redux";
 import store from "./redux/store";
-const Container=styled.div`
+const Container = styled.div`
   text-align: center;
-`
+`;
 
 function App() {
   return (
     <Provider store={store}>
       <Container>
-        <Header/>
-        <Content/>
+        <Header />
+        <Content />
       </Container>
     </Provider>
   );
